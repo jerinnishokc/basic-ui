d34ff88c2cf707359997526d8212674bdd97d21c
Jerin Nishok C <jerinnishok@outlook.com>
2025-12-11 18:14:37 +0530

    added tests and updated the actions with additional step in the job

---
+++ src/App.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import { describe, it, expect } from 'vitest'
+import App from './App'
+
+describe('App', () => {
+  it('renders the "Click on the Vite and React logos to learn more" text', () => {
+    render(<App />)
+    
+    const text = screen.getByText('Click on the Vite and React logos to learn more')
+    expect(text).toBeInTheDocument()
+  })
+})
